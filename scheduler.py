c677b552034090ba297ab03bd2fb4a97c717393e
Hoang Nguyen <hoangngn.26@gmail.com>
2025-09-22 23:38:34 +0700

    set up basic job scheduler

--- scheduler.py
+++ scheduler.py
@@ -5,3 +5,10 @@ from script import run_stock_job
 
 def basic_job(): 
   print('Job started at:', datetime.now())
+
+# Run every minute 
+schedule.every().minute.do(run_stock_job) 
+
+while True: 
+  schedule.run_pending()
+  time.sleep(1)
